a0769f73c2ee73a37ca464881d6dcf749ce5230b
Pechnkv <m.pechnikov.95@gmail.com>
2024-12-10 22:31:00 +0900

    created project

---
+++ greetings.py
@@ -0,0 +1,15 @@
+username = "Максим"
+title = "Grade 1"
+content = "Этап 1: Задание 1"
+status = "В разработке"
+created_date = 10-12-2024
+issue_date = 20-12-2024
+
+print ("Имя пользователя:", username)
+print ("Заголовок заметки:", title)
+print ("Описание заметки:", content)
+print ("Статус заметки:", status)
+print ("Дата создания заметки:", created_date)
+print ("Дата истечения заметки:", issue_date)
+
+
