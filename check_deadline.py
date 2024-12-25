c69629d9afafa8e3a18780804d46d2c3da5ff069
Pechnkv <m.pechnikov.95@gmail.com>
2024-12-25 18:44:47 +0900

    check deadline

--- check_deadline.py
+++ check_deadline.py
@@ -1,2 +1,22 @@
-#Обработка дедлайнов
+# Обработка дедлайнов
+import datetime
+from datetime import datetime,date
 
+
+datelist_1 = input('Input your date 1: ').split('.')
+datelist_1 = input('Input your date 2: ').split('.')
+
+
+
+# created_date = input("Введите дату создания заметки в формате 11-11-2024: ")
+# issue_date = input("Введите дату редактирования формате 11-11-2024: ")
+# temp_issue_date = issue_date[:5]
+# temp_created_date = created_date[:5]
+# print("Дата создания заметки", temp_created_date)
+# print("Дата изменения заметки", temp_issue_date)
+#
+# date_1 =
+#
+# delloing = temp_issue_date - temp_created_date
+#
+# print(delloing)
